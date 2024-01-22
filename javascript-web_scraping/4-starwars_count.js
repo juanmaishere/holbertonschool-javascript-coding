f4337f67f44bc41c96ae3d036385eb7b1b66c44c
juanmaishere <rsjuanmanuel@hotmail.com>
2024-01-22 17:42:00 +0000

    feat

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const id = process.argv[2];
-const url = "https://swapi-api.hbtn.io/api/films/";
+const url = process.argv[2];
 
 request.get(url, (error, response, body) => {
   if (error) {
