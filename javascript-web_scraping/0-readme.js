b96db545c2aa3d5e7a39d2caf0d1226254d8fbef
juanmaishere <rsjuanmanuel@hotmail.com>
2024-01-22 16:19:11 +0000

    feat

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
-let fs = require('fs');
+const fs = require('fs');
 
-const filepath = process.argv[2]
+const filepath = process.argv[2];
+
 fs.readFile(filepath, 'utf-8', (error, data) => {
   if (error) {
     console.error('Error reading the file:', error.message);
-    process.exit(1)
+    process.exit(1);
   }
   else {
-  console.log(data)
+    console.log(data);
   }
 });
