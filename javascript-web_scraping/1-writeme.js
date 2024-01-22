4edbe6c96ee220e6ac23a34373d8f8dfad881d29
juanmaishere <rsjuanmanuel@hotmail.com>
2024-01-22 16:28:35 +0000

    feat

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ let fs = require('request');
 const filepath = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filepath, content, utf-8, (err) => {
+fs.writeFile(filepath, content, 'utf-8', (err) => {
   if (err) {
     console.log(err)
   }
